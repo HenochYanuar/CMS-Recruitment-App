46f188f07ce8195de6f46c240b017272eeabd4dc
Henoch <henochyanuar13@gmail.com>
2025-04-26 18:15:20 +0700

    fix(knexfile): fix connection setting

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,15 +1,21 @@
 // Update with your config settings.
 
-const connection = {
-  client: 'pg',
-  connection: {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
+const connection = process.env.DATABASE_URL
+? {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
   }
-}
+: {
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+    }
+  };
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,22 @@
 // Update with your config settings.
+require('dotenv').config()
 
-const connection = {
-  client: 'pg',
-  connection: {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-  }
-}
+const connection = process.env.DATABASE_URL
+  ? {
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    }
+  : {
+      client: 'pg',
+      connection: {
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        database: process.env.DATABASE_NAME,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+      }
+    }
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
