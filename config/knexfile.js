9b732f945dbce614e40a817670c3dd5d9fd1b1eb
Henoch <henochyanuar13@gmail.com>
2025-04-27 20:55:31 +0700

    fix(deploy): fix the setting of  migrations directory

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,24 +1,32 @@
 // Update with your config settings.
+require('dotenv').config()
+
+const path = require('path');
 
 const connection = process.env.DATABASE_URL
-? {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
-    migrations: {
-      directory: './migrations',
-    },
-  }
-: {
-    client: 'pg',
-    connection: {
-      host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
+  ? {
+      client: 'pg',
+      connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      },
+      migrations: {
+        directory: path.join(__dirname, '../migrations'),
+      },
     }
-  };
+  : {
+      client: 'pg',
+      connection: {
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        database: process.env.DATABASE_NAME,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+      },
+      migrations: {
+        directory: path.join(__dirname, '../migrations'),
+      },
+    }
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
